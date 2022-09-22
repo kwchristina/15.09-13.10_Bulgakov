1fcfe4e1a82aba90bdd47805e3c7e07e61370e93
Kristina <contact@ckwon.ru>
2022-09-22 15:51:00 +0900

    Задача 4

---
+++ Task06/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4. Найти максимальное из трех чисел
+
+Console.Write("Enter the first number:  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number:  ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the third number:  ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = (num1);
+if (num2 < num1) max = num1;
+else if (num3 < num2) max = num2;
+else if (num3 < num1) max = num1;
+else if (num1 < num3) max = num3;
+Console.WriteLine($"Maximum number: {max}");
