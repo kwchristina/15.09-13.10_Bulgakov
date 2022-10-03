52ae954d569ff0e858fed157366ff147f6acfdb7
Kristina <contact@ckwon.ru>
2022-10-03 19:45:22 +0900

    Задача10.Семинар2

---
+++ Task09/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Enter three-digit integer:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+string secondDigit = Convert.ToString(num);
+
+Console.WriteLine($"Second digit: {secondDigit[1]}");
+
