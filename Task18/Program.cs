a054bdaddfc7291a53d1dbfd2247480dc7719db8
Kristina <contact@ckwon.ru>
2022-10-03 19:29:10 +0900

    Задача15.Семинар2

---
+++ Task18/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Enter the number of week:  ");
+string num = Console.ReadLine();
+if (num == "1,2,3,4,5") Console.WriteLine("Weekday- no");
+else Console.WriteLine("Day off- yes");
+
+
